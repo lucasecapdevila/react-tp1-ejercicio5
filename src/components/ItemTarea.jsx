9b0c99fdda3466d5eea02f7fdc3baefd0662add5
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-10 17:39:14 -0300

    se reemplaza elemento <i> de font awesome por componente React

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,10 +1,12 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button, ListGroupItem } from 'react-bootstrap'
 
 const ItemTarea = ({tarea, eliminarTarea}) => {
   return (
     <ListGroupItem className='d-flex align-items-center justify-content-between'>
       {tarea.nombreTarea}
-      <Button variant='secondary' className='me-2' onClick={() => eliminarTarea(tarea.id)}><i className="fa-solid fa-trash"></i></Button>
+      <Button variant='secondary' className='me-2' onClick={() => eliminarTarea(tarea.id)}><FontAwesomeIcon icon={faTrash} /></Button>
     </ListGroupItem>
   )
 }
