c5aa652317baa61cdd7c1904cd3ce88075adbc27
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-23 14:32:47 -0300

    se cambia modelo de tabla de tareas por una lista

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,20 +1,20 @@
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faFilePen, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button } from 'react-bootstrap'
+import { Button, ListGroupItem } from 'react-bootstrap'
 
 const ItemTarea = ({tarea, eliminarTarea, setTareaAEditar}) => {
   return (
-    <tr key={tarea._id} className='d-flex align-items-center justify-content-between'>
-      <td>{tarea.nombreTarea}</td>
-      <td>
+    <ListGroupItem key={tarea._id} className='d-flex align-items-center justify-content-between'>
+      {tarea.nombreTarea}
+      <div>
         <Button variant='warning' className='me-2' onClick={() => setTareaAEditar(tarea)}>
-          <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon icon={faFilePen} />
         </Button>
         <Button variant='secondary' className='me-2' onClick={() => eliminarTarea(tarea._id)}>
           <FontAwesomeIcon icon={faTrash} />
         </Button>
-      </td>
-    </tr>
+      </div>
+    </ListGroupItem>
   )
 }
 
--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,14 +1,12 @@
-import { Button, Table } from 'react-bootstrap'
+import { Button, ListGroup } from 'react-bootstrap'
 import ItemTarea from './ItemTarea'
 
 export default function ListaTareas({listaTareas, eliminarTarea, setTareaAEditar}) {
   return (
     <div className='d-flex flex-column my-4'>
-      <Table striped>
-        <tbody>
-          {listaTareas.map((tarea, i) => (<ItemTarea key={i} tarea={tarea} eliminarTarea={eliminarTarea} setTareaAEditar={setTareaAEditar} />))}
-        </tbody>
-      </Table>
+      <ListGroup>
+        {listaTareas.map((tarea, i) => (<ItemTarea key={i} tarea={tarea} eliminarTarea={eliminarTarea} setTareaAEditar={setTareaAEditar} />))}
+      </ListGroup>
       <Button variant='danger' className='mt-3 align-self-end'>Eliminar todas las tareas</Button>    
     </div>
   )
